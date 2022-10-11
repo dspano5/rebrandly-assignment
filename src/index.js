f6e71651941ef947cccb00c1681095b8aa643ac0
Davide Spano <davide.spano10@gmail.com>
2022-10-11 23:44:39 +0200

    added header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchComp from './profile/components/SearchComp';
+import properties from "./profile/libs/properties";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <SearchComp/>
+        <div className="header">{properties.labels.title}</div>
+        <div className="container">
+            <SearchComp/>
+        </div>
     </React.StrictMode>
 );
